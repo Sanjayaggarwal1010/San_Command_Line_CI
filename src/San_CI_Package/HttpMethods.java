a1627607c374eb6a394e6c4e4d67adf370cb5cfa
Sanjay <Sanjayaggarwal87@outlook.com>
2017-10-26 07:15:29 +0530

    test

--- src/San_CI_Package/HttpMethods.java
+++ src/San_CI_Package/HttpMethods.java
@@ -46,7 +46,7 @@ public class HttpMethods {
 		System.out.println("Git  change ---- Directly committed");
 		System.out.println("Git  change ---- Directly committed---- today");
 		System.out.println("Git  change ---- Directly committed---- today to test git from command line");
-		System.out.println("CMD");
+		System.out.println("CMD123456789");
 		
 	}
 
